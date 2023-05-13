711911722086992786b7c303473115ed52860f24
Marko <kofolmarko00@gmail.com>
2023-05-13 16:22:53 +0200

    add(components): added dashboard and popup components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { PopupComponent } from './components/popup/popup.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    PopupComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+	FormsModule,
+ BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
