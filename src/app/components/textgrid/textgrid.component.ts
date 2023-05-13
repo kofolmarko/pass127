87f81f05e172d70956c9c4c2af93e2eb81e3b937
Lan <lan.biteznik@gmail.com>
2023-05-13 21:12:46 +0200

    add(textgrid): added text grid boilerplate

--- src/app/components/textgrid/textgrid.component.ts
+++ src/app/components/textgrid/textgrid.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-textgrid',
-  templateUrl: './textgrid.component.html',
-  styleUrls: ['./textgrid.component.scss']
+	selector: 'app-textgrid',
+	templateUrl: './textgrid.component.html',
+	styleUrls: ['./textgrid.component.scss'],
 })
 export class TextgridComponent {
+	inputFields: string[] = ['', '', '', '', '', '', '', ''];
 
+  logInputs() {
+    console.log(this.inputFields);
+  }
 }
