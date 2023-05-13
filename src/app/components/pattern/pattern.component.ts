1534e39445cb766af97187024d1a5e08a41dd8f6
JanRojc <jr3628@student.uni-lj.si>
2023-05-13 18:41:48 +0200

    add(pattern): add boilerplate code for pattern

--- src/app/components/pattern/pattern.component.ts
+++ src/app/components/pattern/pattern.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
+
+
+declare var PatternLock: any;
 @Component({
   selector: 'app-pattern',
   templateUrl: './pattern.component.html',
   styleUrls: ['./pattern.component.scss']
 })
-export class PatternComponent {
 
+export class PatternComponent implements OnInit, AfterViewInit {
+  title = 'pattern-lock';
+  matchedPattern = 5784;
+  isMatched: boolean|null = null;
+
+  ngOnInit(): void {
+    
+  }
+
+  ngAfterViewInit(): void {
+    const me = this;
+    var p = new PatternLock(document.getElementById('lock'), {
+        onPattern: function(pattern:any){
+          if(pattern) {
+            if(me.matchedPattern === pattern) {
+              me.isMatched = true;
+              this.success()
+            } else {
+              me.isMatched = false;
+              this.error()
+            }
+          }
+          
+        }
+    });
+  }
+  
 }
