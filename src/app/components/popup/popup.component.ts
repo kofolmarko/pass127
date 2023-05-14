8bc84da2fcdd37d3f4fa17abd1a855f5a2dce40e
Marko Kofol (Junior/LJ) <marko.kofol@achtzig20.de>
2023-05-14 06:16:55 +0200

    change(modal): changed modal navigation

--- src/app/components/popup/popup.component.ts
+++ src/app/components/popup/popup.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { HashService } from 'src/app/services/hash.service';
 
 @Component({
@@ -6,14 +6,34 @@ import { HashService } from 'src/app/services/hash.service';
 	templateUrl: './popup.component.html',
 	styleUrls: ['./popup.component.scss'],
 })
-export class PopupComponent {
+export class PopupComponent implements OnInit {
+	@Input() selectedParams: any[] = [];
+	@Output() onClosePopup = new EventEmitter<void>();
+
 	private pattern: string = '';
 	private password: string = '';
 	private gridInputs: string[] = [];
-	allModals = 1
-	currentModal = 0;
+	activeModals: any[] = [];
+
+	currentModalId: number = 0;
+	currentModal = ''
 
 	constructor(private hashService: HashService) {}
+
+	ngOnInit(): void {
+		console.log(this.selectedParams)
+		if (this.selectedParams.length > 0) {
+			this.currentModal = this.selectedParams[0];
+		}
+	}
+
+	onClose(): void {
+		this.onClosePopup.emit();
+	}
+
+	isParam(label: string): boolean {
+		return this.selectedParams.includes(label);
+	}
 
 	addPattern(pattern: any) {
 		const resPattern = this.hashService.processPattern(pattern);
@@ -26,15 +46,17 @@ export class PopupComponent {
 	}
 
 	onChangeModal(next: boolean): void {
-		if (next && this.currentModal < this.allModals) {
-			this.currentModal++;
+		if (next) {
+			this.currentModalId++;
+		} else {
+			this.currentModalId--;
 		}
-		else if (!next && this.currentModal > 0) {
-			this.currentModal--;
-		}
+		this.currentModal = this.selectedParams[this.currentModalId];
 	}
 
 	getHashedValue(): void {
+		console.log(this.currentModalId)
+		this.onClose();
 		this.password = this.hashService.hash('input params');
 	}
 }
